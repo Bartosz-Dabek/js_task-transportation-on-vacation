bd88d4d5786f7171b5767acbf8c7a854a2c92194
Bartosz <bartek1098@gmail.com>
2026-03-31 12:24:30 +0200

    Ready to check

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  const basicDiscount = 20;
+  const bestDiscount = 50;
+  const basicCost = pricePerDay * days;
+  const discountedCost = basicCost - basicDiscount;
+  const bestCost = basicCost - bestDiscount;
+  const shortTermRent = 3;
+  const longTermRent = 7;
+
+  if (days < shortTermRent) {
+    return basicCost;
+  }
+
+  if (days >= shortTermRent && days < longTermRent) {
+    return discountedCost;
+  }
+
+  if (days >= longTermRent) {
+    return bestCost;
+  }
 }
 
 module.exports = calculateRentalCost;
